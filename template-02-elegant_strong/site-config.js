1c34fe7e6923da39c5d52321d026c1a31ef23012
StevenArmijo <stevenarmijo11@gmail.com>
2026-09-11 10:09:57 -0700

    Last Commit Before Integrating AicedBot and Newsletter

--- template-02-elegant_strong/site-config.js
+++ template-02-elegant_strong/site-config.js
@@ -7,7 +7,7 @@ const realtorData = {
     heroIntro: "Helping buyers and sellers make confident moves throughout the Antelope Valley while mentoring agents to build the business and life they deserve.",
     images: {
         logo: "assets/images/stephlogo/stephanie-mendoza-logo copy.png",
-        hero: "assets/images/StephMendozaCover2.webp",
+        hero: "assets/images/stephlogo/ChatGPT Image Aug 27, 2026, 09_33_13 AM.png",
         clients: "https://images.unsplash.com/photo-1600566753086-00f18fb6b3ea?auto=format&fit=crop&w=1400&q=88",
         agents: "assets/images/stephlogo/iStock-1329013293.jpg"
     }
